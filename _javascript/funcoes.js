43bfe8a2030e2a42025a3b2fc6a8b39912e252ec
Kelvin Lima <kelvin_lima94@hotmail.com>
2020-05-11 23:45:46 -0300

    projeto html5

---
+++ _javascript/funcoes.js
@@ -0,0 +1,3 @@
+function mudaImagem(imagem) { /*RECEBE UM VALOR DE VARIÁVEL EM (imagem)*/
+    document.getElementById("icone").src = "_imagens/" + imagem + ".png"; /*MUDA O LINK DA TAG <img id="icone">*/
+}
